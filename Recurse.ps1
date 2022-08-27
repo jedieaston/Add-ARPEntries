a49274b7ed6e36d2c9f093566ba7cd8184b7f026
Russell Banks <Russell@bandev.uk>
2022-08-27 20:46:03 +0100

    Add validation and cleanup for Recurse.ps1

--- Recurse.ps1
+++ Recurse.ps1
@@ -1,15 +1,86 @@
+#Requires -Version 5
+
 Param(
-    [Parameter(Mandatory = $true, Position = 0, HelpMessage = "The directory to recurse through.")] 
+    [Parameter(Mandatory = $true, HelpMessage = "The directory to recurse through.")] 
     [String] $DirectoryPath
 )
 
+# Check if Docker is running
+docker ps | Out-Null
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Docker isn't running. Start it up (and make sure it's in Windows mode) before continuing!" -ForegroundColor Red
+    exit
+}
+
+# Check if git is installed
+try {
+    git | Out-Null
+} catch [System.Management.Automation.CommandNotFoundException] {
+    Write-Host "Git is not installed." -ForegroundColor Red
+    exit
+}
+
+# Check if GitHub CLI is installed
+try {
+    gh | Out-Null
+} catch [System.Management.Automation.CommandNotFoundException] {
+    Write-Host "GitHub CLI is not installed." -ForegroundColor Red
+    exit
+}
+
+# If the user has git installed, make sure it is a patched version
+if (Get-Command 'git.exe' -ErrorAction SilentlyContinue) {
+    $GitMinimumVersion = [System.Version]::Parse('2.35.2')
+    $gitVersionString = ((git version) | Select-String '([0-9]{1,}\.){3,4}').Matches.Value.Trim(' ', '.')
+    $gitVersion = [System.Version]::Parse($gitVersionString)
+    if ($gitVersion -lt $GitMinimumVersion) {
+        # Prompt user to install git
+        if (Get-Command 'winget.exe' -ErrorAction SilentlyContinue) {
+            $_menu = @{
+                entries       = @('[Y] Upgrade Git'; '[N] Do not upgrade')
+                Prompt        = 'The version of git installed on your machine does not satisfy the requirement of version >= 2.35.2; Would you like to upgrade?'
+                HelpText      = "Upgrading will attempt to upgrade git using winget`n"
+                DefaultString = ''
+            }
+            switch (Invoke-KeypressMenu -Prompt $_menu['Prompt'] -Entries $_menu['Entries'] -DefaultString $_menu['DefaultString'] -HelpText $_menu['HelpText']) {
+                'Y' { 
+                    Write-Host
+                    try {
+                        winget upgrade --id Git.Git --exact
+                    } catch {
+                        throw [UnmetDependencyException]::new('Git could not be upgraded sucessfully', $_)
+                    } finally {
+                        $gitVersionString = ((git version) | Select-String '([0-9]{1,}\.){3,4}').Matches.Value.Trim(' ', '.')
+                        $gitVersion = [System.Version]::Parse($gitVersionString)
+                        if ($gitVersion -lt $GitMinimumVersion) {
+                            throw [UnmetDependencyException]::new('Git could not be upgraded sucessfully')
+                        }
+                    }
+                 }
+                default { Write-Host; throw [UnmetDependencyException]::new('The version of git installed on your machine does not satisfy the requirement of version >= 2.35.2') }
+            }
+        } else {
+            throw [UnmetDependencyException]::new('The version of git installed on your machine does not satisfy the requirement of version >= 2.35.2')
+        }
+    }
+    # Check whether the script is present inside a fork/clone of jedieaston/Add-ARPEntries repository
+    try {
+        $script:gitTopLevel = (Resolve-Path $(git rev-parse --show-toplevel)).Path
+    } catch {
+        # If there was an exception, the user isn't in a git repo. Throw a custom exception and pass the original exception as an InternalException
+        throw [UnmetDependencyException]::new('This script must be run from inside a clone of the Add-ARPEntries repository', $_.Exception)
+    }
+}
+
 Set-Location $DirectoryPath
-$PackageName = Get-ChildItem -Path $path -Force -Recurse -File | Select-Object -First 1 | Select-Object -ExpandProperty Name
-$PackageName = $PackageName -replace ".installer.yaml","" -replace '\.', ' '
-$CommitTitle = “Add ARP Entries for $PackageName"
+$PackageIdentifier = Get-ChildItem -Path $path -Force -Recurse -File | Select-Object -First 1 | Select-Object -ExpandProperty Name
+$PackageIdentifier = $PackageIdentifier -replace ".installer.yaml",""
 Get-ChildItem -Recurse -File -Filter *.installer.yaml | ForEach-Object {
+    $PackageVersion = Split-Path $_.PSParentPath -Leaf
+    $CommitTitle = "ARP Entries: $PackageIdentifier version $PackageVersion"
+    $FileHash = Get-ChildItem -Path $_.PSParentPath -Force -Recurse -File | Select-Object -First 1 | Get-FileHash
+    $BranchName = "$PackageIdentifier-$($FileHash.Hash[0..14] -Join '')"
     .$PSScriptRoot/Add-ARPEntries.ps1 $_.PSParentPath -NoDenormalizeInstallerTypes
-    $BranchName = New-Guid
     git switch upstream/master --detach --quiet
     git add .
     git commit --message=$CommitTitle
