b40fddf94fb883adb2bc38f9d5d8beda3237de83
Vedant <83997633+vedantmgoyal2009@users.noreply.github.com>
2022-10-15 07:44:28 +0530

    Some non-breaking changes (#14)
    * Update Bootstrap.ps1
    
    * Update Dockerfile
    
    * Delete EdgeBlocker.cmd
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Bootstrap.ps1
    
    * Update Dockerfile

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -1,39 +1,21 @@
 $ErrorActionPreference = "Stop"
-function Update-EnvironmentVariables {
-      foreach($level in "Machine","User") {
-        [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
-            # For Path variables, append the new values, if they're not already in there
-            if($_.Name -match 'Path$') {
-              $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
-            }
-            $_
-        } | Set-Content -Path { "Env:$($_.Name)" }
-      }
-    }
-
-    function Get-ARPTable {
-      $registry_paths = @('HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*')
-      return Get-ItemProperty $registry_paths -ErrorAction SilentlyContinue |
-           Select-Object DisplayName, DisplayVersion, Publisher, SystemComponent, @{N='ProductCode'; E={$_.PSChildName}} |
-           Where-Object {$null -ne $_.DisplayName -and $_.SystemComponent -ne 1} |
-           Select-Object DisplayName, DisplayVersion, Publisher, ProductCode
-    }
-
+function Get-ARPTable {
+    $registry_paths = @('HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*')
+    return Get-ItemProperty $registry_paths -ErrorAction SilentlyContinue |
+        Select-Object DisplayName, DisplayVersion, Publisher, SystemComponent, @{N='ProductCode'; E={$_.PSChildName}} |
+        Where-Object {$null -ne $_.DisplayName -and $_.SystemComponent -ne 1} |
+        Select-Object DisplayName, DisplayVersion, Publisher, ProductCode
+}
 Set-Service edgeupdate -Status stopped -StartupType disabled ; Set-Service edgeupdatem -Status stopped -StartupType disabled
 Write-Host "Installing the manifest..."
 $originalARP = Get-ARPTable
-Write-Host $args
-&.\wingetdev.exe install ${args} -m .\manifest\
+.\wingetdev.exe install -m .\manifest\
 if ($LASTEXITCODE -ne 0)
 {
     Write-Host $LASTEXITCODE
     Write-Host "Something went wrong."
     New-Item .\out\uhoh
 }
-Write-Host @'
---> Refreshing environment variables.
-'@
-Update-EnvironmentVariables
 Write-Host @'
 --> Writing ARP changes to output.json.
 '@
--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,25 @@
 # On Windows 10? Change the image to mcr.microsoft.com/windows:20H2
 FROM mcr.microsoft.com/windows/server:ltsc2022 as wingettest
 USER ContainerAdministrator
-WORKDIR C:\\wingetdev\\
-ADD [ "https://aka.ms/vs/16/release/vc_redist.x64.exe", "C:\\" ]
-RUN C:\vc_redist.x64.exe /install /passive /norestart /log C:\TEMP\vc_redist.log
-ADD ["https://github.com/jedieaston/winget-build/releases/latest/download/wingetdev.zip", "C:\\"]
-RUN powershell -Command "Expand-Archive -LiteralPath C:\wingetdev.zip -DestinationPath C:\wingetdev\ -Force ; mv C:\\wingetdev\\AppInstallerCLI\\* C:\\wingetdev\\"
-ADD ["Bootstrap.ps1", "Bootstrap.ps1"]
-RUN wingetdev.exe settings --Enable LocalManifestFiles 
+WORKDIR C:\\wingetdev
+
+# add files to working directory
+ADD Bootstrap.ps1 .
+ADD https://aka.ms/vs/16/release/vc_redist.x64.exe .
+ADD https://github.com/jedieaston/winget-build/releases/latest/download/wingetdev.zip .
+
+# Install vc_redist, enable local manifests in wingetdev
+SHELL [ "powershell", "-Command" ]
+RUN Expand-Archive -LiteralPath .\wingetdev.zip -DestinationPath . -Force ; Move-Item .\AppInstallerCLI\* .
+RUN .\vc_redist.x64.exe /install /passive /norestart /log .\vc_redist.log
+RUN .\wingetdev.exe settings --enable LocalManifestFiles
+
 # Make sure Edge won't change the ARP table at runtime.
-RUN wingetdev.exe install -s winget Microsoft.Edge
-ADD ["EdgeBlocker.cmd", "EdgeBlocker.cmd"]
-RUN cmd /c EdgeBlocker.cmd /B
-RUN powershell -Command "rm -r -force 'C:\\Program Files (x86)\\Microsoft\\EdgeUpdate\\'"
-RUN powershell -Command "Set-Service edgeupdate -Status stopped -StartupType disabled ; Set-Service edgeupdatem -Status stopped -StartupType disabled"
+# RUN .\wingetdev.exe install -s winget Microsoft.Edge
+RUN New-Item -Path HKLM:\\SOFTWARE\\Microsoft\\EdgeUpdate -Force
+RUN New-ItemProperty -Path HKLM:\\SOFTWARE\\Microsoft\\EdgeUpdate -Name DoNotUpdateToEdgeWithChromium -Value 1 -PropertyType DWord -Force
+RUN Set-Service -Name edgeupdate -Status Stopped -StartupType Disabled # stop edgeupdate service
+RUN Set-Service -Name edgeupdatem -Status Stopped -StartupType Disabled # stop edgeupdatem service
+RUN Remove-Item -Force -Recurse 'C:\\Program Files (x86)\\Microsoft\\EdgeUpdate\\'
+
 ENTRYPOINT ["powershell", "-File", "Bootstrap.ps1"]
